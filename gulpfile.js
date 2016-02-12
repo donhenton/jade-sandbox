28f29cbaf3c558f0761e04e24c8f0a60f7b34ba6
Don Henton <donhenton@gmail.com>
2016-02-12 08:04:20 -0600

    live reload work 3

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ var tap = require('gulp-tap');
 var nodemon = require('gulp-nodemon');
 var path = require('path');
 var gutil = require('gulp-util');
-var runSequence = require('run-sequence');
+//var runSequence = require('run-sequence');
+var exec = require('child_process').exec;
 //var rimraf = require('gulp-rimraf');
 //var rev = require("gulp-rev");
 //var revReplace = require("gulp-rev-replace");
@@ -16,37 +17,8 @@ var appDependencies = require('./package.json').dependencies;
 var buildDir = '.';
 var publicDir = buildDir + '/public';
 global._publicDir = publicDir;
-//require('./browserify-task.js');
-var watchItems = ['./views/**/*.jade', './routes/**/*.js'];
-var pageURL = 'http://localhost:3000';
 
-//gulp.task('watch', function () {
-//
-//
-//
-//    watch(watchItems, function (events, done) {
-//
-//
-//        console.log("processing change in watched items");
-////         livereload.reload(pageURL);
-//
-//    });
-//
-//    watch('./sass/**/*.scss', function (events, done) {
-//
-//        gulp.src('./sass/**/*.scss')
-//                .pipe(sass().on('error', sass.logError))
-//                .pipe(concat('style.css'))
-//                // .pipe(uglifycss())
-//                .pipe(gulp.dest('./public/stylesheets/'))
-//                .on('finish', function ( ) {
-//                    console.log("processing change in css");
-//                    //   livereload.reload(pageURL);
-//                });
-//
-//    });
-//
-//});
+
 
 
 gulp.task('sass', function () {
@@ -63,9 +35,15 @@ gulp.task('sass', function () {
 });
 
 
-gulp.task('run-server', function () {
 
-  livereload.listen();
+
+
+/**
+ * one way of full end monitoring
+ */
+gulp.task('run-monitor', function () {
+
+    livereload.listen();
 
     nodemon(
             {
@@ -83,10 +61,10 @@ gulp.task('run-server', function () {
                         }
 
                         //
-                        
+
                     })
 
-                return tasks;
+                    return tasks;
 
                 }
 
@@ -99,4 +77,55 @@ gulp.task('run-server', function () {
 });
 
 
- 
+////////////////////////////////////////////////////////////////////////////
+
+/*
+ * 2nd way of full monitoring
+ * 
+ */
+
+
+
+var watchItems = ['./views/**/*.jade', './routes/**/*.js','./public/**/*.css'];
+var pageURL = 'http://localhost:3000';
+
+/*
+ * start server
+ */
+gulp.task('server-start', function (cb) {
+    exec('node ./bin/www.js', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
+gulp.task('watch', function () {
+
+    livereload.listen();
+
+    watch(watchItems, function (events, done) {
+
+
+        console.log("processing change in watched items");
+        livereload.reload(pageURL);
+
+    });
+
+    watch('./sass/**/*.scss', function (events, done) {
+
+        gulp.src('./sass/**/*.scss')
+                .pipe(sass().on('error', sass.logError))
+                .pipe(concat('style.css'))
+                // .pipe(uglifycss())
+                .pipe(gulp.dest('./public/stylesheets/'))
+                .on('finish', function ( ) {
+                    console.log("processing change in css");
+                   // livereload.reload(pageURL);
+                });
+
+    });
+
+});
+
+gulp.task('init', ['server-start', 'watch']);
