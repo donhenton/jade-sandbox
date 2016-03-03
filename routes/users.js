fa5235f91b64c74e7ef69fdc07c0613b6ef4b686
Don Henton <donhenton@gmail.com>
2016-03-03 17:10:58 -0600

    inline links second route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', { title: 'Users Page' , 'jsfile':'users_code.js'});
 });
 
 module.exports = router;
