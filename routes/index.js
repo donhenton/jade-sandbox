cb5fe7dc7aadf4ee727fcf8e487efb7346f6def1
Don Henton <donhenton@gmail.com>
2016-03-15 11:19:11 -0500

    added header display on index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ module.exports = function(app)
     
     indexRender = function (req,res)
     {
-        res.render('pages/index', { title: 'Express Main Page' , 'jsfile':'index_code.js'});
+        
+        
+        
+        var headers = [];
+        
+        var keys = Object.keys(req.headers);
+        keys.forEach(function(x)
+        {
+            
+            var stringHead =req.headers[x];
+            if (stringHead.length > 30)
+            {
+                stringHead = stringHead.substring(30)+" ...";
+            }
+            
+            headers.push({key: x, value: stringHead });
+            
+            
+        })
+        
+        
+        
+        
+        
+        res.render('pages/index', { 
+             title: 'Express Main Page' ,
+             header_data: headers,
+            'jsfile':'index_code.js'});
     }
     
     
